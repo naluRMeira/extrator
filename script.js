7d910fa147aa53b44911dce92cc3ef027b016656
naluRMeira <ana.ribeiro.meira@escola.pr.gov.br>
2025-08-28 16:50:31 -0300

    n vcg

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
 const texto = document.querySelector("#entrada-de-texto");
 const campoResultado = document.querySelector('#resultado-palavrahave');
-campoResultado.textContent = texto;
 const palavras = procesaTexto (texto);
 campoResultado.textContent = palavras.join("- ");
 }
