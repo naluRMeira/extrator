e05bd1f3d9c2d29f9b706e906bf546d777436b32
naluRMeira <ana.ribeiro.meira@escola.pr.gov.br>
2025-08-28 16:32:36 -0300

    tfcf

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const botaoMostraPalavras = document.querySelector();
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+const texto = document.querySelector("#entrada-de-texto");
+
+const campoResultado = document.querySelector('#resultado-palavrahave');
+
+campoResultado.textContent = texto;
+
+const palavras = procesaTexto (texto);
+
+campoResultado.textContent = palavras.join("- ");
+}
